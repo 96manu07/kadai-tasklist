94710ac0a01b1782406aba160276cc4da95781c4
mayu.kameda <mywn_17@yahoo.co.jp>
2024-05-27 18:25:46 +0900

    データベース関連のファイルを修正

--- src/models/Tasks.java
+++ src/models/Tasks.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "tasks")
-public class tasks {
+public class Tasks {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
